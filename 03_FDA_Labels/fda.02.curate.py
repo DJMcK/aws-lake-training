06dd395c98e3090cf6323469945f50a9713aa0f2
David J. McKeown <david.j.mckeown@pwc.com>
2019-09-05 08:46:12 -0700

    Update to FDA data processing

--- 03_FDA_Labels/fda.02.curate.py
+++ 03_FDA_Labels/fda.02.curate.py
@@ -6,14 +6,7 @@ BUCKET_CURATED = "djm2-lake-curated"
 
 # Read the JSON data as a data frame
 landed_data = "s3://"+BUCKET_LANDING+"/fda/2019-08-10/drug/label/part-*"
-
-dataframe = spark.read.option(
-    "multiLine",
-    True
-).option(
-    "mode",
-    "PERMISSIVE"
-).json(landed_data)
+dataframe = spark.read.json(landed_data)
 
 # Store refined data as Parquet
 dataframe.write.mode("overwrite").parquet("s3://"+BUCKET_CURATED+"/fda/drug/label/")
