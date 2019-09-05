5ac656610efb64754202e4d605bddfa1d2aecf61
David J. McKeown <david.j.mckeown@pwc.com>
2019-09-04 19:39:53 -0700

    Update DBpedia code for configuration inputs

--- 05_DBpedia/dbpedia.curate.py
+++ 05_DBpedia/dbpedia.curate.py
@@ -1,5 +1,11 @@
 from pyspark.sql.types import IntegerType, StructField, StructType, StringType
 
+####
+# CONFIGURATION
+####
+BUCKET_LANDING = "djm2-lake-landing"
+BUCKET_CURATED = "djm2-lake-curated"
+
 ###
 #  Given that the source files (CSV files) does not have a header row
 #  then we won't be able to infer the schema / data structure.
@@ -16,7 +22,7 @@ schema = StructType(
 ###
 #  Bring in the train data and convert to Parquet
 ###
-training_data = "s3://djm2-lake-landing/dbpedia/train.csv.gz"
+training_data = "s3://"+BUCKET_LANDING+"/dbpedia/train.csv.gz"
 training_dataframe = spark.read.csv(training_data, header=False, schema=schema)
 training_dataframe.count()
 training_dataframe.write.mode("overwrite").parquet("s3://djm2-lake-curated/dbpedia/train/")
@@ -24,7 +30,7 @@ training_dataframe.write.mode("overwrite").parquet("s3://djm2-lake-curated/dbped
 ###
 #  Bring in the test data and convert to Parquet
 ###
-test_data = "s3://djm2-lake-raw/dbpedia/test.csv.gz"
+test_data = "s3://"+BUCKET_LANDING+"/dbpedia/test.csv.gz"
 test_dataframe = spark.read.csv(test_data, header=False, schema=schema)
 test_dataframe.count()
-test_dataframe.write.mode("overwrite").parquet("s3://djm2-lake-curated/dbpedia/test/")
+test_dataframe.write.mode("overwrite").parquet("s3://"+BUCKET_CURATED+"/dbpedia/test/")
